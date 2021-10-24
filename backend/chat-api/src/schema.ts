65bf6be2c7f1ac564d4db1b7e95a40e2da79780d
leeja <leejanzen91@gmail.com>
2021-10-24 15:57:54 -0700

    stuff

---
+++ backend/chat-api/src/schema.ts
@@ -0,0 +1,29 @@
+import { makeExecutableSchema } from 'graphql-tools';
+import merge = require('lodash/merge');
+import Scalars from './scalars';
+
+// Modules
+import Messages from './resolvers/Messages';
+
+const Modules = {
+  typeDefs: [
+    Messages.typeDefs,
+  ],
+  resolvers: merge(
+    Messages.resolvers,
+  )
+};
+
+
+const schema = makeExecutableSchema({
+  typeDefs: [
+    ...Scalars.declarations,
+    ...Modules.typeDefs
+  ],
+  resolvers: {
+    ...Scalars.resolvers,
+    ...Modules.resolvers
+  }
+});
+
+export default schema;
