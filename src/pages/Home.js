7a1c8e589deeaa18b6a3b3f31d6b65141dfbfd05
leeja <leejanzen91@gmail.com>
2021-10-24 01:51:36 -0700

    Front page styling

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,34 @@
 import React from "react";
-import Chat from "../components/Chat";
+// import Chat from "../components/Chat";
+import pomilogo from "../assets/pomilogo.png";
+import w_pomi_txt from "../assets/w_pomi_txt.png";
+import book from "../assets/book.png";
+import { Link } from "react-router-dom";
+import styles from "./Home.module.scss";
 
-const Home = () => <Chat />;
+const Home = () => {
+  return (
+    <div className={styles.home}>
+      <div className={styles.col}>
+        <img src={pomilogo} alt="logo" className={styles.logo} />
+        <img src={w_pomi_txt} alt="Pomi" className={styles.logotext} />
+
+        <Link to="/chat">
+          <div className={styles.button}>Start a Chat</div>
+        </Link>
+        <img src={book} alt="Research book" className={styles.bookicon} />
+      </div>
+      <div className={styles.col}>
+        <iframe
+          src="https://www.youtube.com/embed/E7wJTI-1dvQ"
+          frameborder="0"
+          allow="autoplay; encrypted-media"
+          allowfullscreen
+          title="video"
+        />
+      </div>
+    </div>
+  );
+};
 
 export default Home;
